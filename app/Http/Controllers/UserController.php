dbc13ed1e14da28dffabcb0548f25b1ff0b88bb8
Sushiland123 <163489848+Sushiland123@users.noreply.github.com>
2025-02-06 18:09:37 -0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(UserResource::collection($users), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Str::random(8); // Le colocamos una contraseña por defecto
+
+        $user = User::create($data);
+        
+        return response()->json(UserResource::make($user), 201);
+    }
+}
